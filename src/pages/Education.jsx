2b958f51b0e299c784c78438359796b441840482
Jon Klein <jonklein2021@gmail.com>
2024-12-02 09:41:40 -0500

    draft of education section

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import harvard from '../assets/harvard.png';
+import cambridge from '../assets/cambridge.png';
+import gottingen from '../assets/gottingen.png';
 import '../styles/Education.scss';
 
 function Education() {
   return (
     <div className="section education">
+      <div className="header">
         <h1>Education</h1>
+        <hr style={{ color: "#f2f2f2", width: "80%" }} />
+      </div>
+      <div className="content">
+        <div className="left">
+          <img src={harvard} />
+          <p>It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</p>
+          <img src={cambridge} />
+        </div>
+        <div className="right">
+          <p>It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</p>
+          <img src={gottingen} />
+          <p>It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</p>
+        </div>
+      </div>
     </div>
   )
 }
